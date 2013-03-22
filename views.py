ce916b1c2993dcdfce9b0efcc49940c57acc8ec7
Rodrigo Delduca <rodrigo.delduca@ortizgaming.com>
2013-03-21 21:14:01 -0400

    Refactoring

---
+++ views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from bottle import default_app, route, request, error
+from google.appengine.ext.webapp.util import run_wsgi_app
+from memorize import memorize
+
+
+
+@route('/')
+@memorize
+def index():
+    return "ok"
+
+@route('/test/:param1/:param2')
+@memorize
+def test(param1, param2):
+    return param1
+
+def main():
+    run_wsgi_app(default_app())
+
+if __name__ == "__main__":
+    main()
+
